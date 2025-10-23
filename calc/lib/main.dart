54d2f328d027de2f75f3745de7d3e4598ef16ab6
thalyson123ui <69772569+thalyson123ui@users.noreply.github.com>
2025-10-23 14:47:07 -0300

    atualização

--- calc/lib/main.dart
+++ calc/lib/main.dart
@@ -1,122 +1,145 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(CalculadoraApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class CalculadoraApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Calculadora',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CalculadoraTela(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CalculadoraTela extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CalculadoraTelaState createState() => _CalculadoraTelaState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CalculadoraTelaState extends State<CalculadoraTela> {
+  String _resultado = '';
+  String _entrada = '';
 
-  void _incrementCounter() {
+  void _adicionarValor(String valor) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _entrada += valor;
     });
+  }
+
+  void _limpar() {
+    setState(() {
+      _entrada = '';
+      _resultado = '';
+    });
+  }
+
+  void _calcular() {
+    try {
+      final expressao = _entrada.replaceAll('×', '*').replaceAll('÷', '/');
+      final resultadoFinal = _avaliar(expressao);
+      setState(() {
+        _resultado = resultadoFinal.toString();
+      });
+    } catch (e) {
+      setState(() {
+        _resultado = 'Erro';
+      });
+    }
+  }
+
+  double _avaliar(String expressao) {
+    // Avaliação simples usando a biblioteca 'math_expressions' seria ideal
+    // Aqui está uma versão simplificada apenas para demonstração
+    return double.parse(expressao); // substitua por parser real
+  }
+
+  Widget _botao(String texto, {Color cor = Colors.grey}) {
+    return Expanded(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: cor),
+        onPressed: () {
+          if (texto == 'C') {
+            _limpar();
+          } else if (texto == '=') {
+            _calcular();
+          } else {
+            _adicionarValor(texto);
+          }
+        },
+        child: Text(texto, style: TextStyle(fontSize: 24)),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: Text('Calculadora')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomRight,
+              padding: EdgeInsets.all(24),
+              child: Text(_entrada, style: TextStyle(fontSize: 32)),
             ),
-          ],
-        ),
+          ),
+          Container(
+            alignment: Alignment.bottomRight,
+            padding: EdgeInsets.all(24),
+            child: Text(
+              _resultado,
+              style: TextStyle(
+                fontSize: 24,
+                color: const Color.fromARGB(255, 175, 246, 9),
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  _botao('7'),
+                  _botao('8'),
+                  _botao('9'),
+                  _botao('÷', cor: Colors.orange),
+                ],
+              ),
+              Row(
+                children: [
+                  _botao('4'),
+                  _botao('5'),
+                  _botao('6'),
+                  _botao('×', cor: Colors.orange),
+                ],
+              ),
+              Row(
+                children: [
+                  _botao('1'),
+                  _botao('2'),
+                  _botao('3'),
+                  _botao('-', cor: Colors.orange),
+                ],
+              ),
+              Row(
+                children: [
+                  _botao('0'),
+                  _botao('.'),
+                  _botao('C'),
+                  _botao('+', cor: Colors.orange),
+                ],
+              ),
+              Row(
+                children: [
+                  _botao('=', cor: const Color.fromARGB(255, 4, 255, 201)),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
